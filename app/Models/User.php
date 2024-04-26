b45b2d9fbf6a01b38e8e04aba6986ee7234763f3
intan <intaniamentari@gmail.com>
2024-04-26 23:24:02 +0800

    update migration and model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,13 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'invoice_id',
+        'username',
+        'address',
+        'age',
+        'phone',
+        'gender',
+        'category',
     ];
 
     /**
