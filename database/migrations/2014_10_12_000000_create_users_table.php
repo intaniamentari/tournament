bf9826e98a1e1fedf21bb7ff97805dae2bb773ae
intan <intaniamentari@gmail.com>
2024-04-27 01:31:28 +0800

    add softdelete for user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -27,6 +27,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
